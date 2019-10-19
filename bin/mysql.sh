6a8164cb068d67faad197f4023e4f13dbb8a1071
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2019-10-19 18:17:21 +0900

    script to use mysql:5.6

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MySQL playground with the official image on Docker Hub
 # https://hub.docker.com/_/mysql
-IMAGE=mysql:5
+IMAGE=${IMAGE:-mysql:5}
 NAME=mysql-5
 NETWORK=mysql-nw
 docker network create ${NETWORK}
